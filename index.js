46b865ef21b5d35aa8ca7a24ea05dd19ee164d3c
Airous89 <50056994+Airous89@users.noreply.github.com>
2019-08-14 11:41:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function displayResults(responseJson, maxResults){
       div.setAttribute('class', 'ingredients');
       div.setAttribute('id', `ingredients${i}`);
         for(let j = 1; j < 21; j++){
-            div.appendChild(<li>${ingredientString}</li>)
+            div.appendChild(`<li>${ingredientString}</li>`)
             $('#result-list').append(div )
            };
       };
